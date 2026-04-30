f222879c6bfcb93084b1280dbda309a915acbc61
Maria Luiza Cunha <marialuizacunhadsilva@gmail.com>
2026-04-30 19:50:49 -0300

    versão finalizado

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -4,10 +4,10 @@ import os, sys, random
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import protocolo as p
 
-LOSS_SIMUL = False
+LOSS_SIMUL = True
 LOSS_RATE = 0.01
 
-# requisicao do usuario (@IP_Servidor:Porta_Servidor/nome_do_arquivo.ext)
+# leitura da requisicao do usuario (@IP_Servidor:Porta_Servidor/nome_do_arquivo.ext)
 def file_parser():
     if len(sys.argv) < 2: 
         print("Erro de requisicao: @IP_Servidor:Porta_Servidor/nome_do_arquivo.ext")
@@ -18,32 +18,34 @@ def file_parser():
     ip_server, port_server = addr.split(":")
 
     return ip_server, int(port_server), file_name
-
-# implementar erro de encontrar arquivo
-
+ 
 class Client: 
     def __init__(self):
         self.socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
         self.segments = {} 
         self.total_segments = None
 
-    def send_request(self, ip, port, file_name): # GET
+    def send_request(self, ip, port, file_name): # requisicao com protocolo da aplicacap
         addr = (ip, port)
-        message = p.get(file_name)
+        message = p.get(file_name) # protocolo GET
         
-        self.socket.sendto(message, (ip, port))
-        
-        self.receive_segments()
+        self.socket.sendto(message, (ip, port)) # envia requisicao
+        self.receive_segments()                 
 
+        if self.total_segments is None:
+            print("\nErro: servidor offline.")
+            sys.exit(1)
+            
         retries = 0
         max_retries = 3
 
+        # enquanto estiver faltando segmentos, pede retransmissao para o servidor. 
         while self.missing_segments() and retries < max_retries:
             last_missing = len(self.missing_segments())
             
             self.request_retransmit(addr)
             self.receive_segments()
-
+            
             if len(self.missing_segments()) == last_missing:
                 retries += 1
                 print(f"Falha na retransmissao")
@@ -57,7 +59,7 @@ class Client:
         data = self.mount_segment()
         self.save_file(file_name, data)
 
-        
+    # recebe segmentos enviados pelo servidor
     def receive_segments(self):
         self.socket.settimeout(p.TIMEOUT)
         while True: 
@@ -72,12 +74,14 @@ class Client:
                 elif msg_type == "DATA":
                     dmount_seg = p.unpack_pkt(recv_seg)
 
-                    if not p.is_corrupt(dmount_seg):
-
+                    if not p.is_corrupt(dmount_seg):  # verifica a integridade de cada segmento
+                        
+                        # simulacao de perda
                         if LOSS_SIMUL and random.random() < LOSS_RATE:
                             print(f"\nSegmento {dmount_seg.seg} descartado.")
-
-                        self.segments[dmount_seg.seg] = dmount_seg
+                            continue
+                        
+                        self.segments[dmount_seg.seg] = dmount_seg  # salva segmentos recebidos corretamente
                         self.total_segments = dmount_seg.total_seg
 
                         file_end = dmount_seg.seg >= self.total_segments - 1
@@ -91,16 +95,16 @@ class Client:
                 break
         
     def mount_segment(self):
-        print("Remotando arquivo.")
+        print(" Remotando arquivo.")
         file_data = b"" # tipo obj de bytes
         
-        data = [self.segments[seq].data for seq in sorted(self.segments.keys())]
-        file_data = b"".join(data)
+        data = [self.segments[seq].data for seq in sorted(self.segments.keys())] # ordena os segmentos
+        file_data = b"".join(data) # remonta os bytes
         
-        print(f"Arquivo remontado.")
+        print(f" Arquivo remontado.")
         return file_data
 
-    # verifica se foi recebido todos os segmentos
+    # verifica se foi recebido todos os segmentos - completude do arquivo
     def missing_segments(self):
         if self.total_segments is None:
             return []
@@ -112,6 +116,7 @@ class Client:
         return list(missing_segs)
 
     def save_file(self, file_name, mount_data):
+        # salva o arquivo localmente
         folder = os.path.join(os.path.dirname(__file__), 'downloads')
         os.makedirs(folder, exist_ok=True)
         full_path = os.path.join(folder, file_name)
@@ -120,9 +125,9 @@ class Client:
             with open(full_path, "wb") as f:
                 f.write(mount_data)
             
-            print("Arquivo salvo")
+            print(" Arquivo salvo.")
             
-            os.system(f"xdg-open {full_path}")  
+            os.system(f"xdg-open {full_path}")  # exibe arquivo (linux)
         
         except OSError as e:
             print(f"Erro ao salvar arquivo: {e}")
@@ -146,7 +151,7 @@ if __name__ == "__main__":
         client.send_request(ip, porta, filename)
     
     except s.timeout:
-        print("Erro: servidor não encontrado")
+        print("Erro: servidor não encontrado.")
     
     finally:
         client.socket.close()
--- servidor/servidor.py
+++ servidor/servidor.py
@@ -1,4 +1,6 @@
 import os, sys, socket as s, threading, math
+from pathlib import Path
+
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import protocolo as p
@@ -12,12 +14,48 @@ class Server:
         self.socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
         self.socket.bind(('', p.PORT))
 
+    def in_listen(self):
+        print("Em escuta . . . ")
+        
+        while True:
+            data, addr = self.socket.recvfrom(p.MAX_DGRAM)
+            thread = threading.Thread (
+            target=self.handle_request,
+            args=(data, addr) )
+            
+            thread.start()
+
     # verifica se o arquivo existe
     def _verify_request_file(self, file_name):
-        path = os.path.join(os.path.dirname(__file__), 'files', file_name)
-        return os.path.exists(path) and os.path.isfile(path)
+        path = Path(__file__).parent / 'files' / file_name
+        return path.is_file()
     
-    # divisao de arquivos em chuncks - MTU
+  
+    # processa requisicao do cliente
+    def handle_request(self, data, addr):
+        request_type, decoded_data = p.decode_message(data)
+
+        if request_type == "GET": 
+            if self._verify_request_file(decoded_data): # se o arquivo existe, envia ele
+                self.send_file(decoded_data, addr)
+                
+            else: 
+                error_msg = p.error(decoded_data)       # envia mensagem de erro definida no protocolo
+                self.socket.sendto(error_msg, addr)    
+        
+        elif request_type == "RTS":
+            self.retransmit_data(decoded_data, addr)
+
+        elif request_type == "ACK":
+            self.ack[addr] = decoded_data
+            
+            if addr in self.ack_events:
+                self.ack_events[addr].set()
+
+        else: 
+            pass   
+
+
     def send_file(self, file_name, addr):
         path = os.path.join(os.path.dirname(__file__), 'files', file_name)
 
@@ -29,18 +67,22 @@ class Server:
 
         self.ack_events[addr] = threading.Event()
 
-        seq_num = 0       
-
-        with open(path, "rb") as f:
+        seq_number = 0       
+        with open(path, "rb") as f:     # divide o arquivo em pedacos
             chunk = f.read(p.MSS)
+            
             while chunk:                              
-                segment = p.make_seg(seq_num, chunk, total_segments)
-                self.segments[addr][seq_num] = segment         
-                seq_num += 1
+                segment = p.make_seg(seq_number, chunk, total_segments)
+                self.segments[addr][seq_number] = segment         
+                seq_number += 1
                 chunk = f.read(p.MSS)
 
         print(f"Transferencia iniciada para IP/Porta: {addr} . . .")
-        # python cliente.py @127.0.0.1:2000/teste1.pdf
+
+        self.window(total_segments, addr)
+
+
+    def window(self, total_segments, addr):
         base = 0
         while base < total_segments:
             max_wnd = min(base + p.WND_SIZE, total_segments)
@@ -56,55 +98,22 @@ class Server:
 
             ack_in_time = self.ack_events[addr].wait(timeout=0.5)
             
-            if ack_in_time and self.ack.get(addr, -1) >= max_wnd - 1:
+            if ack_in_time and self.ack[addr] >= max_wnd - 1:
                 base += p.WND_SIZE
             
             else:
                 print(f"   Reenvio por timeout . . .")
 
-     
-    def handle_request(self, data, addr):
-        request_type, decoded_data = p.decode_message(data)
 
-        if request_type == "GET": 
-            if self._verify_request_file(decoded_data):# se o arquivo existe
-                self.send_file(decoded_data, addr)
-                
-            else: 
-                self.send_error(decoded_data, addr)
-        
-        elif request_type == "RTS":
-            self.retransmit_data(decoded_data, addr)
-
-        elif request_type == "ACK":
-            self.ack[addr] = decoded_data
-            if addr in self.ack_events:
-                self.ack_events[addr].set()
-
-        else: 
-            print("Mensagem desconhecida")
- 
-    def send_error(self, data, addr):
-        self.socket.sendto(p.error(data), addr)
-        
-    def in_listen(self):
-        print("Em escuta . . . ")
-        
-        while True:
-            data, addr = self.socket.recvfrom(p.MAX_DGRAM)
-            thread = threading.Thread (
-            target=self.handle_request,
-            args=(data, addr) )
-            
-            thread.start()
-
+    # transmissao por perda/solicitacao
     def retransmit_data(self, missing_segments, addr): 
         if addr not in self.segments:
             print(f"Cliente {addr} inativo")
             return    
 
         print(f"Retransmitindo . . .")        
-        for seg in missing_segments:
+        
+        for seg in missing_segments:  # procura segmento perdido no dic de segs do servidor e reenvia
             if self.segments[addr].get(seg) is not None:
                 package = p.pack_pkt(self.segments[addr][seg])
                 self.socket.sendto(package, addr)
